55b3781df8489024bde5a48bd1c870db3cd61bf2
matin bahadoran <matinbhdrn77@gmail.com>
2022-04-04 15:45:14 +0430

    Create search

--- books/books/views.py
+++ books/books/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from .models import Book
+
 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
@@ -15,3 +17,16 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = Book.objects.filter(title__icontains='begginers')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q') 
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
